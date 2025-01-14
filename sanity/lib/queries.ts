e424a3c396cce35b98ca06434bd9ac01978240fb
Gaurav Sharma <131950180+gauravsharma5166@users.noreply.github.com>
2025-01-14 21:44:14 +0530

    author authentication

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -34,3 +34,14 @@ export const STARTUPS_BY_ID_QUERY =
 export const STARTUPS_VIEWS_QUERY = defineQuery(`*[_type == "startup" && _id == $id][0]{
     _id, views
 }`);
+
+export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`
+    *[_type == "author" && id == $id][0]{
+        _id,
+        id,
+        name,
+        username,
+        email,
+        image,
+        bio
+}`);
